08af92cc66c3f81f581d5de23fe9b85c6346471d
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-12-20 10:16:22 -0500

    Replace the methods

--- index.js
+++ index.js
@@ -1,16 +1,166 @@
 import express from "express";
+import { prisma } from "./src/prisma/index.js";
 import dotenv from "dotenv";
-import { studentRouter } from "./src/router/student.routes.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-app.get("/health", (_, res) => {
-    res.status(200).json("Success");
+
+app.post("/students", async (req, res) => {
+    const {
+        body: { firstName, lastName, email, className }
+    } = req;
+    if (!firstName || !lastName || !email || !className) {
+        res.status(400).json({
+            message: "All fields are required"
+        });
+
+        return;
+    }
+
+    try {
+        const student = await prisma.student.create({
+            data: {
+                firstName,
+                lastName,
+                email,
+                className
+            }
+        });
+
+        res.status(201).json({
+            data: student
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        });
+    }
 });
-app.use("/students", studentRouter);
+
+app.get("/students/", async (_, res) => {
+    try {
+        const students = await prisma.student.findMany({
+            select: {
+                id: true,
+                firstName: true,
+                lastName: true,
+                email: true,
+                className: true
+            }
+        });
+
+        res.status(200).json({
+            data: students
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+app.get("/students/:id", async (req, res) => {
+    try {
+        const {
+            params: { id }
+        } = req;
+
+        const student = await prisma.student.findUnique({
+            where: {
+                id
+            }
+        });
+
+        if (!student) {
+            res.status(404).json({
+                message: `Student with ID ${id} is not found`
+            });
+            return;
+        }
+
+        res.status(201).json({ data: updateStudent });
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+app.patch("/students/:id", async (req, res) => {
+    const {
+        params: { id },
+        body: { firstName, lastName, email, className }
+    } = req;
+
+    try {
+        const student = await prisma.student.findUnique({
+            where: {
+                id: id
+            }
+        });
+
+        if (!student) {
+            res.status(404).json({
+                message: "Student is not found"
+            });
+        }
+
+        const updateStudent = await prisma.student.delete({
+            where: {
+                id: id
+            },
+            data: {
+                firstName,
+                lastName,
+                email,
+                className
+            }
+        });
+
+        res.status(201).json({ data: updateStudent });
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+app.delete("/students/:id", async (req, res) => {
+    const {
+        params: { id }
+    } = req;
+    try {
+        const student = await prisma.student.findUnique({
+            where: {
+                id
+            }
+        });
+
+        if (!student) {
+            res.status(404).json({
+                message: "Student is not found"
+            });
+            return;
+        }
+
+        await prisma.student.delete({
+            where: {
+                id
+            }
+        });
+
+        res.status(203).send();
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
 
 app.listen(PORT, () => {
     console.log("Server is running", PORT);
--- src/router/student.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { studentController } from "../controller/student.controller.js";
-
-const studentRouter = new Router();
-studentRouter.post("/", studentController.create);
-
-export { studentRouter };
